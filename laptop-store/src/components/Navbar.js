7604b37f5cd5862717365135bbaaaa3786ae7808
Heyu Shen <hshen232@uwo.ca>
2024-11-26 15:46:14 -0500

    Replace content of front-end-bell with new version

--- laptop-store/src/components/Navbar.js
+++ laptop-store/src/components/Navbar.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Box, TextField, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ onSearch }) {
+    const [query, setQuery] = useState("");
+
+    const handleInputChange = (e) => {
+        setQuery(e.target.value);
+    };
+
+    const handleSearch = () => {
+        onSearch(query);
+    };
+
     return (
         <AppBar position="static">
             <Toolbar>
@@ -14,6 +24,25 @@ function Navbar() {
                 <Button color="inherit" component={Link} to="/cart">Cart</Button>
                 <Button color="inherit" component={Link} to="/login">Login</Button>
                 <Button color="inherit" component={Link} to="/admin">Admin</Button>
+
+                {/* 搜索栏部分 */}
+                <Box display="flex" alignItems="center" ml={2}>
+                    <TextField
+                        label="Search"
+                        variant="outlined"
+                        value={query}
+                        onChange={handleInputChange}
+                        size="small"
+                        sx={{ backgroundColor: 'white', borderRadius: 1, mr: 2 }}
+                    />
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={handleSearch}
+                    >
+                        Search
+                    </Button>
+                </Box>
             </Toolbar>
         </AppBar>
     );
